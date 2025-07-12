9b5da7aace115013fc52952d91ee51add0e68c16
Jonathan Guevara <memooguevara@gmail.com>
2025-07-12 14:15:39 -0500

    feat: add gnome extensions

--- nix/home/jguevara/features/desktop/gnome/default.nix
+++ nix/home/jguevara/features/desktop/gnome/default.nix
@@ -1,12 +1,18 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  extensions = with pkgs.gnomeExtensions; [
+    caffeine
+    color-picker
+    just-perfection
+    space-bar
+    tactile
+  ];
+in {
   imports = [
     ../common
     ../common/wayland-wm
   ];
 
-  home.packages = with pkgs; [
-    gnomeExtensions.caffeine
-  ];
+  home.packages = extensions;
 
   dconf.settings = {
     "org/gnome/desktop/peripherals/touchpad" = {
@@ -15,15 +21,21 @@
     "org/gnome/desktop/interface" = {
       color-scheme = "prefer-dark";
     };
+    "org/gnome/mutter" = {
+      dynamic-workspaces = false;
+    };
+    "org/gnome/desktop/wm/preferences" = {
+      num-workspaces = 4;
+    };
     "org/gnome/shell" = {
       favorite-apps = ["google-chrome.desktop" "Alacritty.desktop"];
-      enabled-extensions = [
-        "caffeine@patapon.info"
-      ];
+      enabled-extensions = builtins.map (i: i.extensionUuid) extensions;
     };
-    "org/gnome/shell/extensions/caffeine" = {
-      show-indicator = "always";
-      toggle-shortcut = ["<Control><Alt>c"];
+    "org/gnome/desktop/wm/keybindings" = {
+      switch-to-workspace-1 = ["<Super>1"];
+      switch-to-workspace-2 = ["<Super>2"];
+      switch-to-workspace-3 = ["<Super>3"];
+      switch-to-workspace-4 = ["<Super>4"];
     };
     "org/gnome/settings-daemon/plugins/media-keys" = {
       custom-keybindings = [
@@ -40,6 +52,21 @@
       binding = "<Primary><Alt>b";
       command = "google-chrome-stable";
       name = "Open browser";
+    };
+    "org/gnome/shell/extensions/caffeine" = {
+      show-indicator = "always";
+      toggle-shortcut = ["<Control><Alt>c"];
+    };
+    "org/gnome/shell/extensions/color-picker" = {
+      auto-copy = true;
+      color-picker-shortcut = ["<Control><Alt>p"];
+      enable-shortcut = true;
+      enable-systray = true;
+    };
+    "org/gnome/shell/extensions/just-perfection" = {
+      accessibility-menu = false;
+      animation = 2;
+      world-clock = false;
     };
   };
 }
